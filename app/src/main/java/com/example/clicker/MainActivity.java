51d922af14f1bc631afe6d323ac7c4ee000f11ce
AgeDim <proga778@gmail.com>
2022-05-05 03:00:26 +0300

    Second Commit. Added a new activity.

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -15,35 +13,31 @@ public class MainActivity extends AppCompatActivity {
     ImageButton settings;
     Button click;
 
-
-    int t = 40;
     int c = 0;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        time = (TextView)findViewById(R.id.time);
-        counter = (TextView)findViewById(R.id.counter);
-        settings = (ImageButton)findViewById(R.id.settings);
-        click = (Button)findViewById(R.id.click);
+        time = findViewById(R.id.time);
+        counter = findViewById(R.id.counter);
+        settings = findViewById(R.id.settings);
+        click = findViewById(R.id.click);
         final long[] start = new long[1];
         click.setEnabled(true);
-        click.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(c == 0){
-                    start[0] = System.currentTimeMillis();
-                }
-                c++;
-                if(c == 41){
-                    time.setText("Time : "+ (System.currentTimeMillis() - start[0]));
-                    c=0;
-                    start[0] = System.currentTimeMillis();
-                }
-                counter.setText("Click : "+ c);
+        click.setOnClickListener(view -> {
+            if(c == 0){
+                start[0] = System.currentTimeMillis();
             }
+            c++;
+            if(c == 41){
+                time.setText("Time : "+ (System.currentTimeMillis() - start[0]));
+                c=0;
+                start[0] = System.currentTimeMillis();
+            }
+            counter.setText("Click : "+ c);
         });
 
     }
